af49952434a71cd8b97b40999287fdb8e915bc18
KAHYA Amira <amira.kahya@rte-france.com>
2026-07-29 16:13:13 +0200

    add unit test

--- src/test/java/com/farao_community/farao/ce_merging/merging/process/virtual_hubs/VirtualHubsShiftingTest.java
+++ src/test/java/com/farao_community/farao/ce_merging/merging/process/virtual_hubs/VirtualHubsShiftingTest.java
@@ -35,50 +35,49 @@ import static org.mockito.Mockito.when;
 class VirtualHubsShiftingTest {
     private static final String RESOURCES_PATH = "src/test/resources/virtual_hubs/";
     private static final String NODE_NAME = "XAA_AA1A";
-    private static final long TASK_ID = 1L;
+    private static final long TASK_ID_1 = 1L;
+    private static final long TASK_ID_2 = 2L;
     private static final OffsetDateTime TARGET_DATE = OffsetDateTime.parse("2026-07-29T23:30Z");
 
     @TempDir
     Path tempDir;
-    private MergingTask task;
+
     private CeMergingConfiguration configuration;
 
     @BeforeEach
     void setUp() {
         configuration = mock(CeMergingConfiguration.class);
         when(configuration.getArtifactsDirectoryPath(any())).thenReturn(tempDir.toString());
-        task = new MergingTask();
-        task.setId(TASK_ID);
-        task.getInputs().setTargetDate(TARGET_DATE);
-        task.getConfigurations().setVirtualHubList(List.of(
-                new VirtualHubRecord("CODE", "EICODE_01", NODE_NAME, "DE", "EICODE_02")
-        ));
-
-        final Artifacts artifacts = new Artifacts();
-        artifacts.putFile(
-                ArtifactType.REFERENCE_PROGRAM_FORECAST_FILE,
-                createSavedFile("forecastReferenceProgram.json")
-        );
-        artifacts.putFile(
-                ArtifactType.TGM_FILE_AFTER_RECESSIVITY,
-                createSavedFile("network.uct")
-        );
-        task.setArtifacts(artifacts);
     }
 
     @Test
     void applyVirtualHubFlowsShouldNotShiftDisconnectedVirtualHub() throws FileNotFoundException {
-
-        final Network initialNetwork = Network.read(task.getArtifacts().getFile(ArtifactType.TGM_FILE_AFTER_RECESSIVITY).getPath());
+        final MergingTask task = createTask(TASK_ID_1, "network.uct");
+        final Network initialNetwork = readNetwork(task);
         final DanglingLine initialDanglingLine = getDanglingLine(initialNetwork);
         assertFalse(initialDanglingLine.getTerminal().isConnected());
         assertEquals(0.0, initialDanglingLine.getP0());
-
         final Map<String, Double> virtualHubGaps = VirtualHubsShifting.applyVirtualHubFlows(task, configuration);
-        final Network finalNetwork = Network.read(task.getArtifacts().getFile(ArtifactType.TGM_FILE_AFTER_RECESSIVITY).getPath());
+        final Network finalNetwork = readNetwork(task);
         final DanglingLine finalDanglingLine = getDanglingLine(finalNetwork);
         assertTrue(virtualHubGaps.isEmpty());
         assertEquals(0.0, finalDanglingLine.getP0());
+    }
+
+    @Test
+    void applyVirtualHubFlowsShouldShiftConnectedVirtualHub() throws FileNotFoundException {
+        final MergingTask task = createTask(TASK_ID_2, "network_terminal_connected.uct");
+        final Network initialNetwork = readNetwork(task);
+        final DanglingLine initialDanglingLine = getDanglingLine(initialNetwork);
+        assertTrue(initialDanglingLine.getTerminal().isConnected());
+        assertEquals(0.0, initialDanglingLine.getP0());
+
+        final Map<String, Double> virtualHubGaps = VirtualHubsShifting.applyVirtualHubFlows(task, configuration);
+        final Network finalNetwork = readNetwork(task);
+        final DanglingLine finalDanglingLine = getDanglingLine(finalNetwork);
+        assertFalse(virtualHubGaps.isEmpty());
+        assertEquals(Map.of("DE", 51.0), virtualHubGaps);
+        assertEquals(51.0, finalDanglingLine.getP0());
     }
 
     private DanglingLine getDanglingLine(final Network network) {
@@ -86,6 +85,31 @@ class VirtualHubsShiftingTest {
                 .filter(dl -> NODE_NAME.equals(dl.getPairingKey()))
                 .findFirst()
                 .orElseThrow();
+    }
+
+    private MergingTask createTask(final long taskId, final String networkFile) {
+        final MergingTask task = new MergingTask();
+        task.setId(taskId);
+        task.getInputs().setTargetDate(TARGET_DATE);
+
+        task.getConfigurations().setVirtualHubList(List.of(
+                new VirtualHubRecord("CODE", "EICODE_01", NODE_NAME, "DE", "EICODE_02")
+        ));
+        final Artifacts artifacts = new Artifacts();
+        artifacts.putFile(
+                ArtifactType.REFERENCE_PROGRAM_FORECAST_FILE,
+                createSavedFile("forecastReferenceProgram.json")
+        );
+        artifacts.putFile(
+                ArtifactType.TGM_FILE_AFTER_RECESSIVITY,
+                createSavedFile(networkFile)
+        );
+        task.setArtifacts(artifacts);
+        return task;
+    }
+
+    private Network readNetwork(final MergingTask task) {
+        return Network.read(task.getArtifacts().getFile(ArtifactType.TGM_FILE_AFTER_RECESSIVITY).getPath());
     }
 
     private static SavedFile createSavedFile(final String fileName) {
