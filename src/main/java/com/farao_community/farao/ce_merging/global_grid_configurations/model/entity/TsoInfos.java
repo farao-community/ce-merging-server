790ba7a61ccdd12dc81370fa2cdc164616876ca9
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2026-09-15 11:01:33 +0200

    remove some abstract classes

--- src/main/java/com/farao_community/farao/ce_merging/global_grid_configurations/model/entity/TsoInfos.java
+++ src/main/java/com/farao_community/farao/ce_merging/global_grid_configurations/model/entity/TsoInfos.java
@@ -6,13 +6,15 @@
  */
 package com.farao_community.farao.ce_merging.global_grid_configurations.model.entity;
 
-import com.farao_community.farao.ce_merging.global_grid_configurations.model.abstractions.AbstractTsoInfos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.io.Serializable;
 
@@ -20,7 +22,7 @@ import static jakarta.persistence.GenerationType.AUTO;
 
 @Entity
 @Table(name = "tsoinfos")
-public class TsoInfos extends AbstractTsoInfos implements Serializable {
+public class TsoInfos implements Serializable {
 
     @Id
     @GeneratedValue(strategy = AUTO)
@@ -39,5 +41,36 @@ public class TsoInfos extends AbstractTsoInfos implements Serializable {
 
     public void setRef(final Long ref) {
         this.ref = ref;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getEic() {
+        return eic;
+    }
+
+    public void setEic(final String eic) {
+        this.eic = eic;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
--- src/main/java/com/farao_community/farao/ce_merging/global_grid_configurations/model/entity/VirtualHubsAlignmentCouple.java
+++ src/main/java/com/farao_community/farao/ce_merging/global_grid_configurations/model/entity/VirtualHubsAlignmentCouple.java
@@ -6,13 +6,18 @@
  */
 package com.farao_community.farao.ce_merging.global_grid_configurations.model.entity;
 
-import com.farao_community.farao.ce_merging.global_grid_configurations.model.abstractions.AbstractXNodeCouple;
 import jakarta.persistence.Embeddable;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.io.Serializable;
 
 @Embeddable
-public class VirtualHubsAlignmentCouple extends AbstractXNodeCouple implements Serializable {
+public class VirtualHubsAlignmentCouple implements Serializable {
+
+    private String referenceXNode;
+    private String recessiveXNode;
 
     public VirtualHubsAlignmentCouple() {
     }
@@ -20,5 +25,36 @@ public class VirtualHubsAlignmentCouple extends AbstractXNodeCouple implements S
     public VirtualHubsAlignmentCouple(final String referenceXNode, final String recessiveXNode) {
         this.referenceXNode = referenceXNode;
         this.recessiveXNode = recessiveXNode;
+    }
+
+    public String getReferenceXNode() {
+        return referenceXNode;
+    }
+
+    public void setReferenceXNode(final String referenceXNode) {
+        this.referenceXNode = referenceXNode;
+    }
+
+    public String getRecessiveXNode() {
+        return recessiveXNode;
+    }
+
+    public void setRecessiveXNode(final String recessiveXNode) {
+        this.recessiveXNode = recessiveXNode;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
